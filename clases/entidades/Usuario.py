39c26fd28e13e570f49c005267ffcf0ee171ae5a
82594-GomezWannazSantiago <santiagogomezwannaz@gmail.com>
2021-06-20 19:54:03 -0300

    Commit Inicial
    definición de la clase y los métodos

---
+++ clases/entidades/Usuario.py
@@ -0,0 +1,13 @@
+
+class Usuario:
+    def __int__(self, caducidad, contra, nombre, empleado):
+        self.caducidad = caducidad
+        self.contrasena = contra
+        self.nombre = nombre
+        self.empleado = empleado
+
+    def conocerEmpleado(self):
+        pass
+    
+    def obtenerEmpleado(self):
+        pass
