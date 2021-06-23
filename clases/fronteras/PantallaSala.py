1b72710a16d36c4a890574266f2e83e669d929e1
DrAmor94 <nachov94@gmail.com>
2021-06-22 21:56:32 -0300

    Update PantallaSala.py

--- clases/fronteras/PantallaSala.py
+++ clases/fronteras/PantallaSala.py
@@ -7,10 +7,10 @@ class PantallaSala():
         cant_maxima_visitantes = 'CAPACIDAD MAXIMA: ' + str(cant_max)
         cant_actual_visitantes = 'VISITANTES ACTUALES: ' + str(cant_actual)
         self.window = tk.Tk()
-        self.window.geometry("990x380") 
+        self.window.geometry("990x300") 
         self.window.resizable(0,0)
         self.window.config(background='#1f2f3f')
-        self.window.title('') 
+        self.window.title('PantallaSala') 
         self.current_max_label = tk.Label(text="", font=('ComicSans', 44), fg='#ffffff', bg='#449a66',pady=10,padx=10)
         self.current_max_label.place(x=140, y=160)
         self.current_act_label = tk.Label(text="", font=('ComicSans', 44), fg='#ffffff', bg='#72a922',pady=10,padx=10)
@@ -21,4 +21,5 @@ class PantallaSala():
 
 
        
-
+pantalla = PantallaSala()
+pantalla.act_cant_visitantes(30, 10)
