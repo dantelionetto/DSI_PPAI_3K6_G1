a37c228feff1a6de6ecf67a10137c3368238a130
73540 Agustin Succar <agustin_succar@hotmail.com>
2021-06-23 18:49:16 -0300

    PantallaEntrada

--- clases/fronteras/PantallaEntrada.py
+++ clases/fronteras/PantallaEntrada.py
@@ -1,10 +1,23 @@
 import tkinter
+import mysql.connector
+from mysql.connector import Error
 
-ventana = tkinter.Tk()
+class PantallaEntrada():
+    def act_cant_visitantes(self, cant_max, cant_actual):
+        cant_maxima_visitantes = 'CAPACIDAD MAXIMA: ' + str(cant_max)
+        cant_actual_visitantes = 'VISITANTES ACTUALES: ' + str(cant_actual)
+        self.pantalla = tkinter.Tk()
+        self.pantalla.geometry("990x300")
+        self.pantalla.resizable(0,0)
+        self.pantalla.config(background='#1f2f3f')
+        self.pantalla.title('Pantalla Entrada')
+        self.current_max_label = tkinter.Label(text="", font=('ComicSans', 44), fg='#ffffff', bg='#449a66',pady=10,padx=10)
+        self.current_max_label.place(x=140, y=160)
+        self.current_act_label = tkinter.Label(text="", font=('ComicSans', 44), fg='#ffffff', bg='#72a922',pady=10,padx=10)
+        self.current_act_label.place(x=100, y=50)
+        self.current_max_label.configure(text= cant_maxima_visitantes)
+        self.current_act_label.configure(text=cant_actual_visitantes)
+        self.pantalla.mainloop()
 
-ventana.geometry("600x400")
-
-etiqueta = tkinter.Label(ventana, text = 'Pantalla entrada principal')
-etiqueta.pack()
-
-ventana.mainloop()
+def actualizarVisitantes():
+   pass
